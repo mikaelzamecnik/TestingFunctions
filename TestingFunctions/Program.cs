77c0aa855b267fec3a19cc893e0bf671dc5667cf
Mikael Zamecnik <zami1783@student.ju.se>
2019-03-16 15:10:38 +0100

    TestingFunctions

--- TestingFunctions/Program.cs
+++ TestingFunctions/Program.cs
@@ -6,7 +6,18 @@ namespace TestingFunctions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Func<string, string, string> func = FullName;
+            // Using a function as an argument
+            string name = func("Mikael", "Zamecnik");
+            Console.WriteLine(name);
+            Console.ReadLine();
         }
+        public static string FullName(string a, string b)
+        {
+            string str = string.Format("My name is {0}",
+                (a +" "+ b));
+            return str;
+        }
+
     }
 }
